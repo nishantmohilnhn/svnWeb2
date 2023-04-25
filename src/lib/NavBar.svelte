cfbcc4d91ce12b8a80b0ead7c7ee357843fb830e
nishantmohilnhn <nniisshhaanntt661199@gmail.com>
2023-04-25 15:38:06 +0530

    init

---
+++ src/lib/NavBar.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { fly } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
+    let show = false;
+</script>
+<nav class="flex justify-between bg-pink-800 p-1">
+    <div class="flex items-center gap-2">
+      <img class="h-8 w-10" src="https://cool-cloud-2974.fly.dev/api/files/x5e0bsbfhuqtpd6/8nlbeyzxeox2to5/group_1_wkXMUld7wH.png" alt="logo S V N school " />
+      <p class="text-white">SHISHU VIDYA NIKETAN</p>
+    </div>
+    <div class="mr-2 sm:hidden">
+      <button on:click={()=> show = !show}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6 text-white">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
+        </svg>
+      </button>
+    </div>
+  </nav>
+  {#if show}
+  <div 
+  transition:fly="{{delay:50, duration: 300, x: 100, opacity: 0.5, easing: quintOut}}"
+  class="fixed right-0 h-screen w-5/6 bg-red-300 sm:hidden z-10 ">
+    <div class="ml-11 mt-14 font-semibold text-white">
+      <a class="mt-7 block" href="/">Home</a>
+      <a class="mt-7 block" href="/">Home</a>
+      <a class="mt-7 block" href="/">Home</a>
+      <a class="mt-7 block" href="/">Home</a>
+      <a class="mt-7 block" href="/">Home</a>
+      <a class="mt-7 block" href="/">Home</a>
+    </div>
+  </div>
+  {/if}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,16 @@
 <script>
-    import "../app.css";
+    import NavBar from "$lib/NavBar.svelte";
+import "../app.css";
   </script>
 <div class="min-h-screen flex flex-col">
-  <header class="bg-red-50 p-2">Header</header>
+  <header class=""><NavBar/></header>
 
   <div class="flex-1 flex flex-col sm:flex-row">
-    <main class="flex-1 bg-indigo-100 p-2">  <slot /> </main>
+    <main class="flex-1  p-2">  <slot /> </main>
 
-    <nav class="order-first sm:w-32 bg-purple-200 p-2 hidden sm:block">Left Navigtion bar </nav>
+    <nav class="order-first sm:w-32  p-2 hidden sm:block">Left Navigtion bar </nav>
 
-    <aside class="sm:w-32 bg-yellow-100 p-2 hidden sm:block ">Right Sidebar</aside>
+    <aside class="sm:w-32  p-2 hidden sm:block ">Right Sidebar</aside>
   </div>
 
   <footer class="bg-gray-100 p-2">Footer</footer>
